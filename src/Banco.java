aef78551e547b7653b9220cd49864a5fc0ea4af3
Douglas Estrela <estreladouglas14@gmail.com>
2025-08-14 10:48:10 -0300

    classe ContaBancaria criada

---
+++ src/Banco.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Banco {
+    private ArrayList<ContaBancaria> contas;
+
+    public Banco(ArrayList<ContaBancaria> contas) {
+        this.contas = contas;
+    }
+
+    public boolean CriarConta(String numero, String, double saldoInicial){
+
+    }
+
+    public ContaBancaria buscarConta(String numero){
+
+    }
+
+    public boolean realizarDeposito(String numero, double valor){
+
+    }
+
+    public boolean realizarSaque(String numero, double valor){
+
+    }
+
+    public boolean realizarTransferencia(String origem, String destino, double valor){
+
+    }
+
+    public void exibirSaldo(String numero){}
+
+
+
+}
